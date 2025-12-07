5f6fc0c681a9183a9a997c4ab4eff4e27ae0a071
aquele best <azullvictorhugo10@gmail.com>
2025-12-07 16:12:27 -0300

    authmiddleware

--- src/middlewere/authMiddleware.ts
+++ src/middlewere/authMiddleware.ts
@@ -1,60 +1,58 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt, { JwtPayload } from 'jsonwebtoken';
-
+import { NextFunction, Request, Response } from 'express';
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 
 interface DecodedToken extends JwtPayload {
-    sub: string;
-    "custom:role"?: string;
+    sub: string
+    "custom:role": string
 }
 
 declare global {
     namespace Express {
         interface Request {
             user?: {
-                id: string;
-                role?: string;
+                id: string,
+                role: string
             }
         }
     }
 }
 
-export const authMiddleware = (allowedRules: string[]) => {
-
+const authMiddleware = (allowedRules: string[]) => {
 
     return (req: Request, res: Response, next: NextFunction) => {
-        const token = req.headers.authorization?.split("")[1];
+
+        const token = req.headers['authorization']?.split(' ')[1];
 
         if (!token) {
-            res.status(401).json({ message: "No token provided" });
+            return res.status(401).json({ message: 'No token provided' });
         }
 
         try {
+            const decodedToken = jwt.decode(token as string) as DecodedToken;
 
-            const decoded = jwt.decode(token as string) as DecodedToken;
-            const userRole = decoded["custom:role"] || "";
+            const userRole = decodedToken["custom:role"] || ""
 
-            req.user = {
-                id: decoded.sub,
+             req.user = {
+                id: decodedToken.sub,
                 role: userRole
             }
 
             const hasAccess = allowedRules.includes(userRole.toLowerCase());
 
             if(!hasAccess) {
-                return res.status(403).json({message:"Access denied"});
+                return res.status(403).json({ message: 'Access denied' });
             }
 
+          
+            next();
+
         } catch (error) {
-           return res.status(400).json({ message: "Invalid token" });
+            return res.status(403).json({ message: 'Invalid token' });
         }
 
-        next()
+
 
     }
-
-
-
-
 
 }
